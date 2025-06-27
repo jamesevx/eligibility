54a818c4329bc943706860fcc4836f6e4d8b7e61
jamesevx <james@evxadvisors.com>
2025-06-27 12:06:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ End with a disclaimer.`
 
   try {
     const completion = await openai.chat.completions.create({
-      model: 'gpt-4',
+      model: 'gpt-4-turbo',
       messages,
       temperature: 0.3
     });
